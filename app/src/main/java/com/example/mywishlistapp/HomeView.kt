c19538cadcdd443bd500aee5ffd401b90d480536
Shubham <shubhamsinghmys@gmail.com>
2025-06-16 21:42:06 +0530

    Wish datatype, WishItem()

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,19 +1,29 @@
 package com.example.mywishlistapp
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.mywishlistapp.data.Wish
 
 @Composable
 fun HomeView(paddingValues: PaddingValues) {
@@ -39,5 +49,26 @@ fun HomeView(paddingValues: PaddingValues) {
                 .fillMaxSize()
                 .padding(it)
         ) { }
+    }
+}
+
+
+@Composable
+fun WishItem(wish: Wish, onClick: () -> Unit) {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+        .clickable{
+            onClick()
+        },
+        elevation = CardDefaults.cardElevation(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+            contentColor = Color.Black
+        )) {
+            Column(modifier = Modifier.padding(16.dp)){
+                Text(text = wish.title, fontWeight = FontWeight.Bold)
+                Text(text = wish.description)
+            }
     }
 }
